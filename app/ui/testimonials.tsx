3408a630414c4e5471aa4f6509904449f90d9676
amaniCoding <amanuelfrm@gmail.com>
2025-01-14 17:02:49 +0300

    added some features

--- app/ui/testimonials.tsx
+++ app/ui/testimonials.tsx
@@ -73,7 +73,7 @@ export default function Testimonials() {
               </div>
 
             </div>
-            <div className="min-h-[320px] md:p-0 p-5">
+            <div className="md:p-0 p-5">
               <div className="relative p-6 bg-[#F7FCFC]">
                 <p className="italic text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, omnis sed modi aut eos ab illo totam officiis eum obcaecati necessitatibus reiciendis quod excepturi quos quae autem et similique deserunt.</p>
 
@@ -92,7 +92,7 @@ export default function Testimonials() {
               </div>
 
             </div>
-            <div className="min-h-[320px] md:p-0 p-5">
+            <div className=" md:p-0 p-5">
               <div className="relative p-6 bg-[#F7FCFC]">
                 <p className="italic text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, omnis sed modi aut eos ab illo totam officiis eum obcaecati necessitatibus reiciendis quod excepturi quos quae autem et similique deserunt.</p>
 
@@ -111,7 +111,7 @@ export default function Testimonials() {
               </div>
 
             </div>
-            <div className="min-h-[320px] md:p-0 p-5">
+            <div className=" md:p-0 p-5">
               <div className="relative p-6 bg-[#F7FCFC]">
                 <p className="italic text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, omnis sed modi aut eos ab illo totam officiis eum obcaecati necessitatibus reiciendis quod excepturi quos quae autem et similique deserunt.</p>
 
@@ -130,7 +130,7 @@ export default function Testimonials() {
               </div>
 
             </div>
-            <div className="min-h-[320px] md:p-0 p-5">
+            <div className=" md:p-0 p-5">
               <div className="relative p-6 bg-[#F7FCFC]">
                 <p className="italic text-lg text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, omnis sed modi aut eos ab illo totam officiis eum obcaecati necessitatibus reiciendis quod excepturi quos quae autem et similique deserunt.</p>
 
